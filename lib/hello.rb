ac2f8d54041a28c6500a2671072a7fbe9e819cb0
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-24 19:31:10 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,19 +3,21 @@
 
 def hello_t(array)
   if block_given?
-    i = 0
-
-    while i < array.length
-      yield array[i]
-      i= i +1
+yielded
     end
     array
   else
     puts "Hey! No block was given!"
   end
-end
+
 
 
 # call your method here!
 
 hello_t([1,2,3,4])
+i = 0
+
+while i < array.length
+  yield array[i]
+  i= i +1
+end
